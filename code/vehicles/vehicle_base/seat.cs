5a0fb108c8e774ec2056d5b62fca2882d5e23ef5
PixelQubed <32498714+PixelQubed@users.noreply.github.com>
2021-06-28 21:03:06 +0100

    Initial Upload

---
+++ code/vehicles/vehicle_base/seat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace gamemode_testing.vehicles.vehicle_base
+{
+
+}
